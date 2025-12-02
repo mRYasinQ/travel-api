b4afc22eb86dfb730755c181c4bc16fe27ce5f41
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2025-12-02 23:42:19 +0330

    refactor(drizzle-kit): improve drizzle-kit config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'drizzle-kit';
 
 const drizzleKitConfig = defineConfig({
   dialect: 'mysql',
-  schema: './src/modules/**/*.entity.ts',
+  schema: ['./src/modules/**/*.entity.ts', './dist/modules/**/*.entity.js'],
   out: './drizzle',
   dbCredentials: {
     host: process.env.DB_HOST!,
     port: Number(process.env.DB_PORT),
     database: process.env.DB_NAME!,
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   },
 });
 
