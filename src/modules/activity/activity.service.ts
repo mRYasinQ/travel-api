4add00b2e06891ee649999f8a9984729022bd157
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2025-12-10 00:56:06 +0330

    chore(role): add conflict error document

--- src/modules/activity/activity.service.ts
+++ src/modules/activity/activity.service.ts
@@ -38,24 +38,16 @@ const getActivities = async ({ page, limit, order_by }: ActivitiesQuery) => {
   };
 };
 
-const getActivity = async (id: number) => {
-  const activity = await db.query.activity.findFirst({
-    where: eq(activityEntity.id, id),
-  });
-  if (!activity) throw new AppError(ActivityMessage.NOT_FOUND, 'NOT_FOUND');
-
-  return activity;
-};
-
 const createActivity = async (name: string, image: string) => {
   await db.insert(activityEntity).values({ name, image });
   return;
 };
 
 const updateActivity = async (id: number, payload: UpdateActivityPayload) => {
-  const activity = await getActivity(id);
+  const activity = await db.query.activity.findFirst({ where: eq(activityEntity.id, id), columns: { image: true } });
+  if (!activity) throw new AppError(ActivityMessage.NOT_FOUND, 'NOT_FOUND');
 
-  const [{ affectedRows }] = await db.update(activityEntity).set(payload).where(eq(activityEntity.id, activity.id));
+  const [{ affectedRows }] = await db.update(activityEntity).set(payload).where(eq(activityEntity.id, id));
   if (affectedRows === 0) throw new AppError(CommonMessage.NO_CHANGE_REQUIRED, 'OK');
 
   if (payload.image) void removeFile(UploadFolders.activity, activity.image);
@@ -64,12 +56,11 @@ const updateActivity = async (id: number, payload: UpdateActivityPayload) => {
 };
 
 const deleteActivity = async (id: number) => {
-  const activity = await getActivity(id);
+  const activity = await db.query.activity.findFirst({ where: eq(activityEntity.id, id), columns: { image: true } });
+  if (!activity) throw new AppError(ActivityMessage.NOT_FOUND, 'NOT_FOUND');
 
-  const [{ affectedRows }] = await db.delete(activityEntity).where(eq(activityEntity.id, activity.id));
-  if (affectedRows === 0) return;
-
-  void removeFile(UploadFolders.activity, activity.image);
+  const [{ affectedRows }] = await db.delete(activityEntity).where(eq(activityEntity.id, id));
+  if (affectedRows === 1) void removeFile(UploadFolders.activity, activity.image);
 
   return;
 };
